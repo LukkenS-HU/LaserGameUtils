96abcbafbfac7fbc19ac6084380969e292fe37d5
LukkenS <stijn.lukken@student.hu.nl>
2022-11-09 15:15:47 +0100

    Move initialization logic to 'initialize()'

--- src/ClockPin.h
+++ src/ClockPin.h
@@ -22,25 +22,30 @@ namespace crt
         {
             periph_module_enable(PERIPH_LEDC_MODULE);
 
+            initialize();
+        }
+
+        void initialize()
+        {
             // Set up timer
             ledc_timer_config_t ledc_timer = {
-               .speed_mode = ledcMode,
-               .duty_resolution = LEDC_TIMER_1_BIT,     // We need a clock, not PWM so 1 bit is enough.
-               .timer_num = ledcTimer,
-               .freq_hz = freqHz,                // Clock frequency, 38kHz
-               .clk_cfg = LEDC_AUTO_CLK
+                    .speed_mode = ledcMode,
+                    .duty_resolution = LEDC_TIMER_1_BIT,     // We need a clock, not PWM so 1 bit is enough.
+                    .timer_num = ledcTimer,
+                    .freq_hz = freqHz,                // Clock frequency, 38kHz
+                    .clk_cfg = LEDC_AUTO_CLK
             };
             ledc_timer_config(&ledc_timer);
 
             // Set up GPIO PIN
             ledc_channel_config_t channel_config = {
-              .gpio_num = gpio,        // GPIO pin
-              .speed_mode = ledcMode,
-              .channel = ledcChannel,
-              .intr_type = LEDC_INTR_DISABLE,
-              .timer_sel = ledcTimer,
-              .duty = 1,
-              .hpoint = 0
+                    .gpio_num = gpio,        // GPIO pin
+                    .speed_mode = ledcMode,
+                    .channel = ledcChannel,
+                    .intr_type = LEDC_INTR_DISABLE,
+                    .timer_sel = ledcTimer,
+                    .duty = 1,
+                    .hpoint = 0
             };
             ledc_channel_config(&channel_config);
 
@@ -55,7 +60,6 @@ namespace crt
         void disable()
         {
             ledc_timer_pause(ledcMode, ledcTimer);
-
         }
 
         void stop()
